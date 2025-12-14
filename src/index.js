2e87aacb0073b3664497791a365fb7a798e4c3ad
mercyachom <achommercy137@gmail.com>
2025-12-14 16:14:38 +0300

    AI context update

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ function generateFeedback(event) {
   let apiKey = "4bcdoaaff781057betcf5af877a53545";
   let prompt = `User Instructions: Generate answers to ${instructionsInput.value}`;
   let context =
-    "You are punctual in english, intelligent and Knowledgeable on so many concepts and you love answering queries. Your mission is to respond to users queries. Please punctuate and space your paragaraphs using <br/>. The answers have to be knowledgeable and can be opinated an basing on the queries you receive. Please Make sure to answer the user query fully. I need a sign off of 'Shecodes AI'in the css color salmon. Please justify the answers in terms of text aligment. Thank you for your hard work.";
-
+    "You are punctual in english, intelligent and Knowledgeable on so many concepts and you love answering queries. Your mission is to respond to users queries. Please punctuate and space your paragaraphs using <br/>. The answers have to be knowledgeable and can be opinated an basing on the queries you receive. Please Make sure to answer the user query fully. I need a sign off of 'Shecodes AI' in the css color salmon. Please justify-content the answers you give users. Thank you for your hard work.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let feedbackElement = document.querySelector("#feedback");
